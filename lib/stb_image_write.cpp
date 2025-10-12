412f49cd57cfb155de1dc21aee8536263d53f65c
Pavel Kupert <disturbedelk@gmail.com>
2025-10-12 12:41:01 +0700

    Remove leftover comments

--- lib/stb_image_write.cpp
+++ lib/stb_image_write.cpp
@@ -8,9 +8,9 @@
 
 // static accessors
 #ifdef STB_IMAGE_WRITE_STATIC
-STBIWDEF int get_stbi_write_tga_with_rle();				// defaults to true; set to 0 to disable RLE
-STBIWDEF int get_stbi_write_png_compression_level();	// defaults to 8; set to higher for more compression
-STBIWDEF int get_stbi_write_force_png_filter();			// defaults to -1; set to 0..5 to force a filter mode
+STBIWDEF int get_stbi_write_tga_with_rle();	
+STBIWDEF int get_stbi_write_png_compression_level();
+STBIWDEF int get_stbi_write_force_png_filter();
 
 STBIWDEF void set_stbi_write_tga_with_rle(int value);
 STBIWDEF void set_stbi_write_png_compression_level(int value);
@@ -46,6 +46,5 @@ STBIWDEF void set_stbi_write_force_png_filter(int value)
 {
 	stbi_write_force_png_filter = value;
 }
-
 #endif
 #endif
